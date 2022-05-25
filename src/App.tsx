b3adb799926c671859fb9bfa5883cd5473bed9cc
fortunato-sply <lucasfortunato2530@gmail.com>
2022-05-24 21:17:32 -0300

    Uploading project

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
+
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+import { NewTransactionModal } from './components/NewTransactionModal';
+
+import { GlobalStyle } from "./styles/global";
+
+Modal.setAppElement('#root');
+
+export function App() {
+  // Modal properties
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
+  return (
+    <>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+      <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
+      <GlobalStyle />
+    </>
+  );
+}
